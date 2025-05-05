15b7cf67d54f1bfe7aa52049ff3a208ff2b1a7f4
AsusKardi <asuskardi2@gmail.com>
2025-05-05 19:08:52 +0300

    Map points redesign have started
    Didn't make inspector cnahges

--- Assets/Scripts/RewardManager.cs
+++ Assets/Scripts/RewardManager.cs
@@ -19,22 +19,44 @@ public class RewardManager: MonoBehaviour
         return new List<Item> (_rewardData.GetItemsDictionary(int.MaxValue, mine.MineType).Keys);
     }
     public List<Item> RewardList() => RewardList(_mine);
+    
     private Item[] GetReward (float rewardPoints)
     {
         Dictionary<Item, float> potentialReward = _rewardData.GetItemsDictionary(rewardPoints, _mine.MineType);
-        List<Item> reward = new();
-        List<Item> keys = new List<Item>(potentialReward.Keys);
-        Item chosenRewardItem;
+        List<Item> reward = new List<Item>();
+        Item chosenRewardItem=null;
 
-        if (keys.Count > 0)
-            while (rewardPoints > 0)
+        float weightsSum = 0f;
+        foreach (var pricedItem in potentialReward) {
+            if (pricedItem.Value!=0f)
+                weightsSum+=1.0f/pricedItem.Value;
+            else
+                potentialReward.Remove (pricedItem.Key);
+        }
+
+        if (potentialReward.Count == 0 || weightsSum == 0f)
+            return reward.ToArray();
+
+        while (rewardPoints > 0)
+        {
+            float randomFloat=Random.value*weightsSum;
+            foreach (KeyValuePair <Item, float> pricedItem in potentialReward)
             {
-                chosenRewardItem = keys[Random.Range(0, keys.Count)];
-                rewardPoints -= potentialReward[chosenRewardItem];
-                if (rewardPoints > 0)
-                    reward.Add(chosenRewardItem.Clone());
+                float currentWeight =1.0f/pricedItem.Value; 
+                if (randomFloat<currentWeight)
+                {
+                    chosenRewardItem=pricedItem.Key;
+                    break;
+                } else {
+                    randomFloat-=currentWeight;
+                }
             }
-
+            rewardPoints -= potentialReward[chosenRewardItem];
+            if (rewardPoints > 0)
+                reward.Add(chosenRewardItem.Clone());
+            chosenRewardItem=null;
+        }
+        
         return reward.ToArray();
     }
 
