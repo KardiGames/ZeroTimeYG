e06321685f33c32ce3db90bc7937a81a6f6a0510
AsusKardi <asuskardi2@gmail.com>
2025-06-24 18:19:10 +0300

    JS lib installing
    -to connect to browser
    -to YG SDK initialization
    +actual index.html in root of the project

---
+++ Assets/Scripts/Yandex/Yandex.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Runtime.InteropServices;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class Yandex : MonoBehaviour
+{
+
+
+    [DllImport("__Internal")]
+    private static extern void Hello();
+
+    [SerializeField] TextMeshProUGUI _nameText;
+    [SerializeField] RawImage _photo;
+
+    public void HelloButton()
+    {
+        Hello();
+    }
+
+    private void SetName(string name) { _nameText.text = name; }
+    private void SetPhoto(string url)
+    {
+        StartCoroutine(DownloadImage(url));
+    }
+
+    private IEnumerator DownloadImage(string mediaUrl)
+    {
+        UnityWebRequest request = UnityWebRequestTexture.GetTexture(mediaUrl);
+        yield return request.SendWebRequest();
+        if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
+        {
+            Debug.Log(request.error);
+        }
+        else
+        {
+            _photo.texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
+        }
+
+    }
+}
